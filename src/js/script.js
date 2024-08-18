70e26020bfaf0a92fa4f11f569dd9a3337065736
kuroptevdb <kuroptevdb@gmail.com>
2024-08-18 20:06:44 +0300

    add smooth scroll to section

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,29 @@
-const hamburgerBtn = document.querySelector(".hamburger")
-const closeMenuBtn = document.querySelector(".menu__close")
-const menu = document.querySelector(".menu")
+const hamburgerBtn = document.querySelector(".hamburger");
+const closeMenuBtn = document.querySelector(".menu__close");
+const menu = document.querySelector(".menu");
 
 hamburgerBtn.addEventListener("click", () => {
-  menu.classList.add("menu_active")
-})
+  menu.classList.add("menu_active");
+});
 
 closeMenuBtn.addEventListener("click", () => {
-  menu.classList.remove("menu_active")
-})
+  menu.classList.remove("menu_active");
+});
+
+function toSection(nameSection) {
+  const section = document.querySelector(`#${nameSection}`);
+  const link = document.querySelector(`[href="#${nameSection}"]`);
+
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    section.scrollIntoView({ behavior: "smooth" });
+    navMenu.classList.remove("nav-menu_active");
+    menu.classList.remove("menu_active");
+  });
+}
+
+toSection("skills");
+toSection("about");
+toSection("pricing");
+toSection("contacts");
+
